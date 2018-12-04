4f6efefee72b7c7ba270420b17c746455ba556f1
vladymyr <vladymyr@coderiver.com.ua>
2018-12-05 00:33:02 +0200

    perlin

--- src/js/_sketch.js
+++ src/js/_sketch.js
@@ -8,35 +8,44 @@ var fr;
 
 var particles = [];
 
-var flowfield;
+var flowfield ;
 
 function setup() {
-  createCanvas(800, 600); 
-//   colorMode(RGB, 20, 120, 220);
+  createCanvas(800, 400);
+
+  // colorMode(HSB, 255);
   cols = floor(width / scl);
   rows = floor(height / scl);
   fr = createP('');
 
   flowfield = new Array(cols * rows);
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 1500; i++) {
     particles[i] = new Particle();
   }
-  background(0);
+  background(255);
 }
 
 function draw() {
+  // background(0);
   var yoff = 0;
+
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 1;
+      // var r = noise(xoff, yoff) * 255;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
       var v = p5.Vector.fromAngle(angle);
+      xoff += inc;
+      // fill(r);
+      // rect(x * scl, y * scl, scl, scl);
+      
+      // var v = p5.Vector.fromAngle(0);
       v.setMag(1);
       flowfield[index] = v;
       xoff += inc;
-      stroke(0, 10);
+      stroke(255, 50);
       // push();
       // translate(x * scl, y * scl);
       // rotate(v.heading());
@@ -46,15 +55,16 @@ function draw() {
     }
     yoff += inc;
 
-    zoff += 100
+    zoff += 0.0003;
   }
 
   for (var i = 0; i < particles.length; i++) {
     particles[i].follow(flowfield);
     particles[i].update();
-    particles[i].edges();
     particles[i].show();
-  }
+    particles[i].edges();
+  // }
 
   fr.html(floor(frameRate()));
+}
 }
