d011327ee7ccfc9e6942edbbdf52c9980a573ab3
vladymyr <vladymyr@coderiver.com.ua>
2018-12-05 15:32:01 +0200

    src

--- src/js/_perlin.js
+++ src/js/_perlin.js
@@ -66,40 +66,4 @@
 
 // }
 
-var yoff = 0.0022;        // 2nd dimension of perlin noise
 
-function setup() {
-  createCanvas(710, 400);
-}
-
-function draw() {
-  background(255);
-
-  fill(255);
-  // We are going to draw a polygon out of the wave points
-  // beginShape(); 
-  
-  var xoff = 0.022;       // Option #1: 2D Noise
-  // var xoff = yoff; // Option #2: 1D Noise
-  // var zoff = 10;
-  // Iterate over horizontal pixels
-  for (var x = 1; x <= width; x += 10) {
-    // Calculate a y value according to noise, map to 
-    
-    // Option #1: 2D Noise
-    // var y = map(noise(xoff, yoff), 0, 1, 200,300);
-
-    // Option #2: 1D Noise
-    var y = map(noise(xoff, yoff), 0, 1, 10,300);
-    
-    // Set the vertex
-    vertex(x, y); 
-    // Increment x dimension for noise
-    xoff += 0.022;
-  }
-  // increment y dimension for noise
-  yoff += 0.022;
-  vertex(width, height);
-  vertex(0, height);
-  endShape(CLOSE);
-}
